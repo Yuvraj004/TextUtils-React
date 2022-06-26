95c4bd1f1e74dba9a29a59bf11d9197ac5f8d20d
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2022-06-26 12:16:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with urself
         </a>
       </header>
     </div>
