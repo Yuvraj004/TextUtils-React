6c8336f47f988ead0f8a4f36fd0706c6d599cd41
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2022-07-26 17:41:10 +0530

    damn

--- src/App.js
+++ src/App.js
@@ -2,18 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import { useState } from 'react';
+import Alert from './components/Alert';
 
 //JSX is javascript syntax extension writing js into html using curly brackets
 let name = "Harry";
 function App() {
+  const [mode,setMode]=useState('light');
+  const [alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500)
+  }
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+      showAlert("Dark mode has been enabled","success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled","success");
+    }
+  }
   return (
     //can return only one tag
     <>
-    <Navbar title= "REactlearn"/>
+    <Navbar title= "Reactlearn" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert} />
     <div className="container">
-    <TextForm heading="Enter text to analyze"/>
+    <TextForm showAlert={showAlert} heading="Enter text to analyze" mode={mode}/>
     </div>
-    <h1>Hello {name}</h1>
+    <About mode={mode}/>
+    {/* <h1>Hello {name}</h1> */}
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
