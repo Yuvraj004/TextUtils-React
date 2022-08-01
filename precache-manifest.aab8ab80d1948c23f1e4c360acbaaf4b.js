a000d71ca9f234025af974a2559391d717e2fa0f
Yuvraj Chaturvedi <96003202+Yuvraj004@users.noreply.github.com>
2022-08-01 19:43:54 +0530

    Updates

---
+++ precache-manifest.aab8ab80d1948c23f1e4c360acbaaf4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b52971ba232c6a5225de",
+    "url": "/TextUtils-React/static/css/main.261d45dc.chunk.css"
+  },
+  {
+    "revision": "b52971ba232c6a5225de",
+    "url": "/TextUtils-React/static/js/main.b52971ba.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils-React/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "7704322fd39edb65a59d",
+    "url": "/TextUtils-React/static/js/2.7704322f.chunk.js"
+  },
+  {
+    "revision": "0d79c3c2a5762a692cdb",
+    "url": "/TextUtils-React/static/js/runtime~main.0d79c3c2.js"
+  },
+  {
+    "revision": "b44865b43ce50d578d1389333b29f5c3",
+    "url": "/TextUtils-React/index.html"
+  }
+];
